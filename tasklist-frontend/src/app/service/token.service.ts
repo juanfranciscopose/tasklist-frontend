bc49a1edfe44e4f9c4dfb858ff6dff40112ecd76
jfp1989 <juanfranciscopose@gmail.com>
2020-08-06 12:52:08 -0300

    desarrollo del token serivce donde manejo el session storage.

--- tasklist-frontend/src/app/service/token.service.ts
+++ tasklist-frontend/src/app/service/token.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { ResolveStart } from '@angular/router';
+
+// browser const => aplication session storage
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUsername';
+const AUTHORITIES_KEY = 'AuthAuthorities';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenService {
+  constructor() {}
 
-  constructor() { }
+  public setToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+  public getToken(): string {
+    return sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public setUsername(username: string): void {
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, username);
+  }
+  public getUsername(): string {
+    return sessionStorage.getItem(USERNAME_KEY);
+  }
+
+  public setAuthorities(authorities: string[]): void {
+    window.sessionStorage.removeItem(AUTHORITIES_KEY);
+    // is an array (authorities parameter) must be converted to json
+    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): string[] {
+    const roles: string[] = [];
+    if (sessionStorage.getItem(AUTHORITIES_KEY)) {
+      JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)).forEach(
+        (authority) => {
+          roles.push(authority.authority);
+        }
+      );
+    }
+    return roles;
+  }
+
+  public logOut(): void {
+    window.sessionStorage.clear();
+  }
 }
