89c3027f3d2b2f3b51eabb5e34fc3cf4a8f71e88
jfp1989 <juanfranciscopose@gmail.com>
2020-08-06 13:05:21 -0300

    desarrollo de auth service: request de creacion de usuario y login

--- tasklist-frontend/src/app/service/auth.service.ts
+++ tasklist-frontend/src/app/service/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserRequest } from '../model/user-request';
+import { LoginRequest } from '../model/login-request';
+import { Credential } from '../model/credential';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private url = 'http://localhost:8080/auth/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public storeUser(userRequest: UserRequest): Observable<any> {
+    return this.httpClient.post<any>(this.url + 'users', userRequest);
+  }
+
+  public login(loginRequest: LoginRequest): Observable<Credential> {
+    return this.httpClient.post<Credential>(this.url + 'login', loginRequest);
+  }
 }
