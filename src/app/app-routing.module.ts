c5190b2ab754b9075b5b761232f5bc7bc65194a9
jfp1989 <juanfranciscopose@gmail.com>
2020-08-14 20:22:28 -0300

    desarrollo de guard y uso en el app-router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,35 @@ import { TaskCrudComponent } from './task-crud/task-crud.component';
 import { OneTaskCardComponent } from './task-crud/one-task-card/one-task-card.component';
 import { HomeComponent } from './home/home.component';
 import { TableComponent } from './task-crud/table/table.component';
+import { AuthGuardService as guard } from './guards/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'public-tasks', component: TableComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'tasks', component: TaskCrudComponent},
-  {path: 'tasks/:id', component: OneTaskCardComponent, pathMatch: 'full'},
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'public-tasks',
+    component: TableComponent,
+    canActivate: [guard],
+    data: { expectedRole: ['admin', 'user'] },
+  },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'tasks',
+    component: TaskCrudComponent,
+    canActivate: [guard],
+    data: { expectedRole: ['admin', 'user'] },
+  },
+  {
+    path: 'tasks/:id',
+    component: OneTaskCardComponent,
+    pathMatch: 'full',
+    canActivate: [guard],
+    data: { expectedRole: ['admin', 'user'] },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { TokenService } from '../service/token.service';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
+  realRol: string;
+  constructor(private router: Router, private tokenService: TokenService) {}
+  // passes only if the required role matches the current one
+  public canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    const expectedRoles: string[] = route.data.expectedRole;
+    const roles = this.tokenService.getAuthorities();
+    this.realRol = 'user';
 
-  constructor() { }
+    roles.forEach((rol) => {
+      if (rol === 'ROL_ADMIN') {
+        this.realRol = 'admin';
+      }
+    });
+
+    if (
+      expectedRoles.indexOf(this.realRol) === -1 ||
+      !this.tokenService.getToken()
+    ) {
+      alert('usted no tiene permiso para ir a esa url');
+      this.router.navigateByUrl('home');
+      return false;
+    } else {
+      return true;
+    }
+
+  }
 }
